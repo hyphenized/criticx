86fb7b77f8a8f5722f278e058df92f48b7a88804
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-19 10:47:52 -0500

    add reverse action to critic body migration

--- db/migrate/20200519153435_change_column_body_on_critic.rb
+++ db/migrate/20200519153435_change_column_body_on_critic.rb
@@ -1,5 +1,9 @@
 class ChangeColumnBodyOnCritic < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :critics, :body, :text
+  end
+
+  def down
+    change_column :critics, :body, :string
   end
 end
