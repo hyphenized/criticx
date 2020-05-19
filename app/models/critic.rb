2d0f751128a090c2fa637aaa7020e612d2bf3f5a
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-19 11:10:46 -0500

    adds critic model validation

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,3 +1,9 @@
 class Critic < ApplicationRecord
   belongs_to :game
+  validates :username, presence: true, length: { minimum: 2, maximum: 12 }, format: {
+    with: /\A[a-zA-Z0-9]+\z/,
+    message: "only alphanumeric allowed",
+  }
+  validates :body, presence: {"Please write something!"}
+  validates :title, presence: {"Please write something!"}
 end
