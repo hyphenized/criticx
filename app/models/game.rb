49f93de05f07913e880f74564eee053212853ca6
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-19 10:58:12 -0500

    adds game model validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ApplicationRecord
   has_many :critics, dependent: :destroy
+  validates :name, presence: true
+  validates :genre, presence: true, inclusion: { in: ["Simulator", "Adventure", "Strategy", "Role-playing (RPG)", "Shooter", "Fighting", "Sport"] }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :release_date, presence: true
 end
