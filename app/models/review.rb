d883dd405bc2abcb612e3124d6411c7f0c747d63
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-25 09:29:44 -0500

    adds update review count callback

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :parent, polymorphic: true
+
+  after_create :inc_review_count
+  after_destroy :dec_review_count
+
+  private
+
+  def inc_review_count
+    review_count = user.review_count || 0
+    user.update(review_count: review_count + 1)
+  end
+
+  def dec_review_count
+    review_count = user.review_count
+    user.update(review_count: review_count - 1)
+  end
 end
