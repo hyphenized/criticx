8dc6c4876cfef8d91a728c7ebfb66ebba0d3bdee
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-27 16:26:34 -0500

    updated auth logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: :show
-  get 'home/index'
+  get '/my_profile', to: 'users#show'
 
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
