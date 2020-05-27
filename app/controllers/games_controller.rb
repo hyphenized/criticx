be5fef1428df75d03d6215e3b408fe00faa976d8
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-27 04:10:58 -0500

    added views for model new and create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,4 +6,32 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def edit
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :summary, :release_date, :category, :rating, :parent_id)
+  end
 end
