bb88b2510f5745d46601d1d7e8f6c537be074a10
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-27 02:14:23 -0500

    updated platform category enum

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -4,14 +4,11 @@ class Platform < ApplicationRecord
 
   has_and_belongs_to_many :games
   enum category: {
-    "PC (Microsoft Windows)": 0,
-    "PlayStation 4": 1,
-    "Xbox One": 2,
-    "Google Stadia": 3,
-    "Nintendo Switch": 4,
-    "Wii U": 5,
-    "Linux": 6,
-    "Mac": 7,
-    "PlayStation 3": 8,
+    console: 0,
+    arcade: 1,
+    platform: 2,
+    operating_system: 3,
+    portable_console: 4,
+    computer: 5,
   }
 end
