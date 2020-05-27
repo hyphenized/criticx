cb88697c94203281667cf84a958305c70fbce05b
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-27 03:12:42 -0500

    seeds initial data to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,89 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+def load_json(fname)
+  path = File.expand_path(fname, __dir__)
+  File.open(path) { |f| JSON.parse(f.read) }
+end
+
+def load_companies
+  puts "Loading Companies..."
+  load_json('companies.json').each do |company|
+    data = {
+      name: company["name"],
+      country: company["country"],
+      start_date: Date.parse(company["start_date"]),
+    }
+    Company.find_or_create_by!(data)
+  end
+end
+
+def load_genres
+  puts "Loading Genres..."
+  load_json('genres.json')["genres"].each do |genre|
+    Genre.create!(name: genre)
+  end
+end
+
+def load_platforms
+  puts "Loading Platforms..."
+  load_json('platforms.json').each do |platform|
+    Platform.find_or_create_by!(platform)
+  end
+end
+
+def load_games
+  puts "Loading Games..."
+  load_json('games.json').each do |game|
+    next if [game["rating"], game["name"], game["category"]].any?(&:nil?)
+
+    data = {
+      name: game["name"],
+      summary: game["summary"],
+      release_date: Date.parse(game["release_date"]),
+      category: game["category"],
+      rating: game["rating"],
+    }
+
+    if game["parent"].nil?
+      new_game = Game.new(data)
+    else
+      parent = Game.find_by(name: game["parent"])
+      new_game = Game.new(**data, parent: parent)
+    end
+
+    unless game["platforms"].nil?
+      game["platforms"].each do |platform|
+        new_game.platforms << Platform.find_by!(platform)
+      end
+    end
+
+    unless game["genres"].nil?
+      game["genres"].each do |genre|
+        new_game.genres << Genre.find_by!(name: genre)
+      end
+    end
+
+    # Persist new game
+    new_game.save!
+
+    unless game["involved_companies"].nil?
+      game["involved_companies"].each do |involved_company|
+        company = Company.find_by!(name: involved_company["name"])
+        data = {
+          company: company,
+          developer: involved_company["developer"],
+          publisher: involved_company["publisher"],
+        }
+        new_game.involved_companies.create!(data)
+      end
+    end
+  end
+end
+
+load_companies
+load_genres
+load_platforms
+load_games
