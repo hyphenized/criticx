da9adab9274951fe754ec252e1ee53c34ea4e26f
_hyphen <3930353-_hyphen@users.noreply.gitlab.com>
2020-05-27 12:34:41 -0500

    added destroy link

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -34,6 +34,9 @@ class CompaniesController < ApplicationController
   end
 
   def destroy
+    company = Company.find(params[:id])
+    company.destroy
+    redirect_to companies_path
   end
 
   private
